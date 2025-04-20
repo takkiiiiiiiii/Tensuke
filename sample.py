b14c2b5b9e9a8e12469ddc8697c88ca78774ae44
Yudai Takihara <yudai2002brazil@gmail.com>
2025-04-20 16:46:31 +0900

    chenage code

--- sample.py
+++ sample.py
@@ -3,9 +3,13 @@ from flask import Flask, abort, request
 from linebot.v3.webhook import (
     WebhookHandler
 )
+
+import uuid
+
 from linebot.v3.exceptions import (
     InvalidSignatureError
 )
+
 from linebot.v3.messaging import (
     Configuration,
     ApiClient,
@@ -13,12 +17,21 @@ from linebot.v3.messaging import (
     ReplyMessageRequest,
     TextMessage
 )
+
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage,
+    ImageMessage,
+    BroadcastRequest,
+)
+
 from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
-
-
     
 
 import sys, os
@@ -29,8 +42,10 @@ from unko import detailWeatherInfo, basicWeatherInfo
 
 app = Flask(__name__)
 
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('0f17045277cb53fda29d6254debd6ba2')
+configuration = Configuration(access_token='KaqoxEaoGKSdDgQP4BbTl9lUTgPTSW6hagMZcYWOjgg5/SaAW+0UAxjjAPgjqDcgMOeE4MaUsaI5vXANAyy98nifd8cokohRxTg/08zpsQKafEV+sqIe1Rn0/UbvZuV02/tWVVgq0BK6G9CGKHu/oAdB04t89/1O/w1cDnyilFU=')
+
+
 
 
 @app.route("/callback", methods=['POST'])
@@ -55,9 +70,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
-        #相手の送信した内容で条件分岐して回答を変数に代入
+        jsonBasicData = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/070000.json"
+        basicInfo = basicWeatherInfo(jsonBasicData)
         if event.message.text == 'グー':
-            msg = 'パー'
+            msg = basicInfo
         elif event.message.text == 'チョキ':
             msg = 'グー'
         elif event.message.text == 'パー':
@@ -73,6 +89,26 @@ def handle_message(event):
             )
         )
 
+# 新しく/pushエンドポイントを作成して、サーバー側からプッシュメッセージを送信する
+@app.route("/broadcast", methods=['POST'])
+def push_message():
+    with ApiClient(configuration) as api_client:
+        try:
+            line_bot_api = MessagingApi(api_client)
+            jsonBasicData = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/070000.json"
+            basicInfo = basicWeatherInfo(jsonBasicData)
+            x_line_retry_key = str(uuid.uuid4())
+            line_bot_api.broadcast_with_http_info(
+                BroadcastRequest(
+                    messages=[TextMessage(text=basicInfo)]
+                    ),
+                    x_line_retry_key=x_line_retry_key
+                )
+            return {'status Code': 200, 'body': 'Push message sent successfully'}
+        except Exception as e:
+            print("Exception when calling MessagingApi->broadcast: %s\n" % e)
+            return {'status Code': 500, 'body': f'Error: {e}'}
+
 
 if __name__ == "__main__":
     jsonBasicData = "https://www.jma.go.jp/bosai/forecast/data/overview_forecast/070000.json"
@@ -83,5 +119,3 @@ if __name__ == "__main__":
     print(f'詳細情報: {detailInfo}')
     port = int(os.getenv("PORT", 7777))
     app.run(host="0.0.0.0", port=port, debug=False)
-
-
